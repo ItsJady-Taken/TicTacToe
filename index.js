27f1176d3f931bce544aa790d62404e3b8de9dd9
David <davidkhun2004@gmail.com>
2024-05-28 14:57:14 +0700

    Status Figuring out how this game will function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 const cells = document.querySelectorAll('.cell');
 
+// create player and asign marker
 const player = (name, marker) =>{
     return { name, marker };
 }
-console.log(player('Jady', 'X'))
+
 // object gameBoard, manage the board
 const gameBoard = {
     board: Array(9).fill(null),
@@ -23,13 +24,45 @@ const gameBoard = {
             return true;
         }
         else {
-            return false;
+            return false; 
         }
     }
 }
-console.log(gameBoard.getBoard)
+
 // Control the game state
+const DisplayController =( function() {
+    const player1 = player('player1', 'X');
+    const player2 = player('player2', 'O');
+    let currentPlayer = player1;
+    let gameActive = true;
+    // all the winning combinations
+    const winCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    const cellClick = (cell) => {
+        const index = cell.target.getAttribute('data-index');
+        cell.target.textContent = currentPlayer.marker;
+        switchPlayer()
+    }
 
-const DisplayController = {
+    const switchPlayer = () => {
+        if(currentPlayer == player1) {
+            currentPlayer = player2;
+        }
+        else {
+            currentPlayer = player1;
+        }
+    }
 
-}
+    cells.forEach((cell)=>{
+        cell.addEventListener('click', cellClick)
+    })
+})();
+
